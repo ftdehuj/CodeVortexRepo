4d7cac954745e61a44c9f3bd7b3a1836c9a089d6
ftdehuj <js1aden0411@hotmail.com>
2024-05-11 21:01:24 +0700

    commit

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-function singleNumber(nums) {
-  let result = 0;
-  for (const num of nums) {
-    result ^= num;
+function levelOrderBottom(root) {
+  if (!root) return [];
+  const result = [];
+  const queue = [root];
+  while (queue.length) {
+    const levelSize = queue.length;
+    const currentLevel = [];
+    for (let i = 0; i < levelSize; i++) {
+      const node = queue.shift();
+      currentLevel.push(node.val);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    result.unshift(currentLevel);
   }
   return result;
 }
