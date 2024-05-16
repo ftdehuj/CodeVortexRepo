3bc92e5eccf7aa0532df44080c36324edb0daa31
ftdehuj <js1aden0411@hotmail.com>
2024-05-16 22:44:24 +0700

    init project

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-function getMax(arr) {
-  return Math.max(...arr);
-}
+const selectionSortRecursive = (arr, start = 0) => {
+  if (start >= arr.length - 1) {
+    return arr;
+  }
+  let minIndex = start;
+  for (let i = start + 1; i < arr.length; i++) {
+    if (arr[i] < arr[minIndex]) {
+      minIndex = i;
+    }
+  }
+  if (minIndex !== start) {
+    [arr[start], arr[minIndex]] = [arr[minIndex], arr[start]];
+  }
+  return selectionSortRecursive(arr, start + 1);
+};
